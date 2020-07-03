9c367adb73cd40bb3f19770f1f0f293de2ee557f
raj <rjkumr@gmail.com>
2020-07-03 14:10:35 -0500

    draft

--- src/main/scala/com/secureworks/analytics/accesslog/TopVisitorsNUrl.scala
+++ src/main/scala/com/secureworks/analytics/accesslog/TopVisitorsNUrl.scala
@@ -1,8 +1,12 @@
 package com.secureworks.analytics.accesslog
 
+import java.text.SimpleDateFormat
+
 import com.secureworks.analytics.utils.Log
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions._
 
 object TopVisitorsNUrl {
 
@@ -10,40 +14,117 @@ object TopVisitorsNUrl {
 
   def main(args: Array[String]): Unit = {
     val param = Param().parse(args)
-    val rdd = param.spark.sparkContext.textFile(param.inputPath)
-    val count = rdd.count()
-    log.info("Count -> " + count)
+    val rdd = param.spark.sparkContext
+      .textFile(param.inputPath).repartition(param.partitionCount)
+    val (validDataRdd, validCount, invalidCount) = parseData(rdd)
+    //val topURLs = getTopNURLs(parsed, param.spark)
+    //val topVisitors = getTopNVisitors(parsed, param.spark)
   }
+
+  def getTopN(rdd: RDD[AccessInfo], spark: SparkSession, colName: String,
+    topN: Int): DataFrame = {
+    import spark.implicits._
+    val df = rdd.toDF
+    val res = df.groupBy(col(colName), col("date"))
+      .count
+      .orderBy($"count".desc)
+      .limit(topN)
+      .withColumn("-", lit("-"))
+    res
+  }
+
+  def parseData(rdd: RDD[String]): (RDD[AccessInfo], Long, Long) = {
+    val mapped: RDD[AccessInfo] = rdd.map(splitLine(_)).cache
+    val validData: RDD[AccessInfo] = mapped.filter(_ != null).cache
+    val validCount = validData.count
+    val invalidCount: Long = mapped.filter(_ == null).count
+    mapped.unpersist()
+    (validData, validCount, invalidCount)
+  }
+
+  def splitLine(line: String): AccessInfo = {
+    // Ex: 199.72.81.55- - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245
+    val rgx = """^(.*)- - \[(.*)\]\s+"([^\s]+) ([^\s]+) ([^\s]+)" (\d+) (\d+)$""".r
+    line match {
+      case rgx(host, dTime, httpMethod, url, version, httpStatus, dataSize) =>
+        getSqlDate(dTime) match {
+          case date: java.sql.Date =>
+            AccessInfo(host = host.trim, dTime = dTime.trim, httpMethod = httpMethod.trim,
+              url = url.trim, version = version.trim, httpStatus = httpStatus.trim,
+              dataSize = dataSize.trim, date = date)
+          case _ =>
+            null
+        }
+      case _ =>
+        null
+    }
+  }
+
+  /*def getSqlDate(dTime: String): java.sql.Date = {
+    val format = new SimpleDateFormat("dd/MMM/yyyy")
+    val rgx = "^([0-9]{2}/[a-zA-Z]{3}/[0-9]{4}).*$".r
+    dTime match {
+      case rgx(dt) =>
+        val parsed = format.parse(dt)
+        new java.sql.Date(parsed.getTime)
+      case _ =>
+        null
+    }
+  }*/
+
+  def getSqlDate(dTime: String): java.sql.Date = {
+    val format = new SimpleDateFormat("dd/MMM/yyyy")
+    try {
+      val parsed = format.parse(dTime)
+      new java.sql.Date(parsed.getTime)
+    }catch {
+      case ex: java.text.ParseException =>
+        null
+    }
+  }
+
 }
+
+case class AccessInfo(
+     host: String,
+     dTime: String,
+     httpMethod: String,
+     url: String,
+     version: String,
+     httpStatus: String,
+     dataSize: String,
+     date: java.sql.Date)
 
 case class Param(
   inputPath: String = null,
+  partitionCount: Int = 8,
+  topN: Int = 10,
   spark: SparkSession = null){
 
   val log: Logger = Log.getLogger(this.getClass.getName)
   val appName: String = "TopVisitorsNUrl"
 
-  def parse(args: Array[String], sparkLocal: Boolean = false): Param = {
+  def parse(args: Array[String]): Param = {
     val parser =
       new scopt.OptionParser[Param](appName) {
         opt[String]("inputPath").required().action { (x, c) =>
-          c.copy(inputPath = x)
+          c.copy(inputPath = x)}
+        opt[Int]("partitionCount").optional().action { (x, c) =>
+          c.copy(partitionCount = x)
         }
       }
     parser.parse(args, Param()) match {
       case Some(param) =>
-        param.copy(spark = getSparkSession(sparkLocal))
+        param.copy(spark = getSparkSession())
       case _ =>
         throw new Exception("Bad arguments")
     }
   }
 
-  private def getSparkSession(sparkLocal: Boolean): SparkSession = {
-    val builder = sparkLocal match {
-      case true => SparkSession.builder.master("local[*]")
-      case false => SparkSession.builder()
-    }
-    val ss = builder.appName(appName)
+  def getSparkSession(): SparkSession = {
+    val ss = SparkSession
+      .builder()
+      .appName(appName)
       .getOrCreate()
     log.info("Spark session created")
     ss
--- src/test/scala/com/secureworks/analytics/accesslog/TopVisitorsNUrlTest.scala
+++ src/test/scala/com/secureworks/analytics/accesslog/TopVisitorsNUrlTest.scala
@@ -1,18 +1,93 @@
 package com.secureworks.analytics.accesslog
 
+import java.text.SimpleDateFormat
+
+import com.secureworks.analytics.utils.Log
+import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.scalatest.FunSuite
 
 class TopVisitorsNUrlTest extends FunSuite{
 
-  SparkSession.builder.master("local[*]").appName("test")
+  // Setting master to local to run the unit tests
+  val spark = SparkSession.builder.master("local[*]")
+    .appName("test")
     .getOrCreate()
+  val log: Logger = Log.getLogger(this.getClass.getName)
 
-  test("-"){
-    val args = Array(
-      "--inputPath",
-      "ftp://anonymous:somepwd@ita.ee.lbl.gov/traces/NASA_access_log_Jul95.gz")
-    TopVisitorsNUrl.main(args)
+  test("getSqlDate(): String to java.sql.Date conversion"){
+    {
+      val dTime = "11/Jul/1995:00:00:01 -0400"
+      assert(TopVisitorsNUrl.getSqlDate(dTime).toString == "1995-07-11")
+    }
+    {
+      val dTime = "11/Ju/1995:00:00:01 -0400"
+      assert(TopVisitorsNUrl.getSqlDate(dTime) == null)
+    }
   }
 
+  test("Parse Single Line: Valid format"){
+    val goodLine =
+      """199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245"""
+    assert(TopVisitorsNUrl.splitLine(goodLine) ==
+      AccessInfo(host = "199.72.81.55", dTime = "01/Jul/1995:00:00:01 -0400",
+        httpMethod = "GET", httpStatus = "200", dataSize = "6245",
+        date = getDate("1995-07-01"), url = "/history/apollo/",
+        version = "HTTP/1.0"))
+  }
+
+  test("Parse Single Line: Invalid format"){
+    val goodLine =
+      """199.72.81.55- - [01/Jul/1995:00:00:01 -0400]"GET /history/apollo/ HTTP/1.0" 200 6245"""
+    assert(TopVisitorsNUrl.splitLine(goodLine) == null)
+  }
+
+  test("Parse Single Line: Invalid Date format"){
+    val goodLine =
+      """199.72.81.55- - [01/Ju/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245"""
+    assert(TopVisitorsNUrl.splitLine(goodLine) == null)
+  }
+
+  test("Parse Input Data (All valid data)"){
+    val rdd = spark.sparkContext.parallelize(sampleData)
+    val (validDataRdd, validCount, invalidCount) = TopVisitorsNUrl.parseData(rdd)
+    assert(validCount == 4)
+    assert(invalidCount == 0)
+  }
+
+  test("Parse Input Data (One Invalid line (Bad date format))"){
+    sampleData :+ """unicomp6.unicomp.net - - [01/Ju/1995:00:00:14 -0400] "GET """ +
+      """/images/KSC-logosmall.gif HTTP/1.0" 200 1204""" match {
+      case sampleData =>
+        val rdd = spark.sparkContext.parallelize(sampleData)
+        val (validDataRdd, validCount, invalidCount) = TopVisitorsNUrl.parseData(rdd)
+        assert(validCount == 4)
+        assert(invalidCount == 1)
+    }
+  }
+
+  test("Parse Data (One Invalid line (Invalid line format))"){
+    sampleData :+ """unicomp6.unicomp.net- - [01/Jul/1995:00:00:14 -0400]"GET """ +
+      """/images/KSC-logosmall.gif HTTP/1.0" 200 1204""" match {
+      case sampleData =>
+        val rdd = spark.sparkContext.parallelize(sampleData)
+        val (validDataRdd, validCount, invalidCount) = TopVisitorsNUrl.parseData(rdd)
+        assert(validCount == 4)
+        assert(invalidCount == 1)
+    }
+  }
+
+  def sampleData(): Array[String] = {
+    Array(
+      """199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245""",
+      """unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985""",
+      """199.120.110.21 - - [01/Jul/1995:00:00:09 -0400] "GET /shuttle/missions/sts-73/mission-sts-73.html HTTP/1.0" 200 4085""",
+      """burger.letters.com - - [01/Jul/1995:00:00:11 -0400] "GET /shuttle/countdown/liftoff.html HTTP/1.0" 304 0""")
+  }
+
+  def getDate(dt: String): java.sql.Date = {
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+    val parsed = format.parse(dt)
+    new java.sql.Date(parsed.getTime)
+  }
 }
